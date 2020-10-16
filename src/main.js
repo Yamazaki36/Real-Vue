f44281e9e635c815611531871a9ee9f877720304
yamazaki <20398801@qq.com>
2020-10-16 15:25:21 +0800

    key

--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+// 阻止启动生产消息
+Vue.config.productionTip = true;
+
+// $mount 手动挂载
+
+/*es5：
+  render:function(createElement){
+    return createElement(App)
+}
+
+  render(createElement){
+    return createElement(App)
+  }
+
+  render(h){
+    return h(App)
+  }
+
+  ES6:
+  render:h=>h(App)
+ */
 
 new Vue({
   render: h => h(App),
